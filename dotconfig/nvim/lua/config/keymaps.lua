ae45d2b3fcc2540db80daa36ac575847063dbc5c
FanZhengang <fzg@localhost.localdomain>
2025-03-03 20:09:48 +0900

    add comment

--- dotconfig/nvim/lua/config/keymaps.lua
+++ dotconfig/nvim/lua/config/keymaps.lua
@@ -1,3 +1,17 @@
+-- nvimnvim     nvimnvim    nvim
+--    nvim      nvim
+--    nvim      nvim
+--    nvim   nvim         nvimnvim       nvimnvimnvimnvim
+--    nvim   nvim            nvim       nvim   nvim   nvim
+--       nvimnvim            nvim       nvim   nvim   nvim
+--       nvim                nvim       nvim   nvim   nvim
+--       nvim           nvimnvimnvim    nvim   nvim   nvim
+
+-- Last Change : 2025-03-03
+--  Maintainer : 樊 振剛（ハン シンゴウ）
+--        Mail : fantaro@gmail.com
+--      Github : https://github.com/fantaro
+
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
