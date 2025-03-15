95d6254e7c5741af41ec7ad724c1c28f63ce22aa
FanZhengang <fzg@localhost.localdomain>
2025-03-15 16:11:00 +0900

    update

--- dotconfig/wezterm/wezterm.lua
+++ dotconfig/wezterm/wezterm.lua
@@ -3,15 +3,17 @@ local wezterm = require 'wezterm'
 -- Hold the configuration.
 local config = wezterm.config_builder()
 
-config.color_scheme = 'Catppuccin Macchiato'
+config.color_scheme = 'Rosé Pine Moon (Gogh)'
 config.enable_wayland = false
 config.font = wezterm.font_with_fallback {
-    { family = 'Hack Nerd Font Mono' },
+    { family = 'FantasqueSansM Nerd Font Mono' },
     { family = 'Noto Sans CJK JP' , weight = 'Medium'},
     { family = 'Noto Sans CJK SC' , weight = 'Medium'},
+    { family = 'Noto Sans CJK TC' , weight = 'Medium'},
+    { family = 'Noto Sans CJK KR' , weight = 'Medium'},
     'Noto Color Emoji'
 }
-config.font_size = 12
+config.font_size = 14
 config.hide_tab_bar_if_only_one_tab = true
 config.initial_cols = 120
 config.initial_rows = 36
