fb013a67d4e219efeaa7aff722069925dd003b5c
FanZhengang <fzg@localhost.localdomain>
2025-03-23 22:48:42 +0900

    update

--- dotconfig/wezterm/wezterm.lua
+++ dotconfig/wezterm/wezterm.lua
@@ -21,6 +21,16 @@ config.switch_to_last_active_tab_when_closing_tab = true
 config.use_ime = true
 config.window_background_opacity = 0.9
 config.window_decorations = "NONE"
+config.window_frame = {
+    border_left_width = '0.2cell',
+    border_right_width = '0.2cell',
+    border_bottom_height = '0.1cell',
+    border_top_height = '0.1cell',
+    border_left_color = 'purple',
+    border_right_color = 'purple',
+    border_bottom_color = 'purple',
+    border_top_color = 'purple',
+}
 
 -- Return the configuration to wezterm
 return config
