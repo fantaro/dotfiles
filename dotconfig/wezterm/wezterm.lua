71de69dcc867e7c0dee7c0933dd79105fcd8abf2
FanZhengang <fzg@localhost.localdomain>
2025-03-24 21:00:08 +0900

    update

--- dotconfig/wezterm/wezterm.lua
+++ dotconfig/wezterm/wezterm.lua
@@ -5,19 +5,15 @@ local config = wezterm.config_builder()
 
 config.color_scheme = 'Rosé Pine Moon (Gogh)'
 config.enable_wayland = false
--- config.font = wezterm.font_with_fallback {
---     { family = 'FantasqueSansM Nerd Font Mono' },
---     { family = 'Noto Sans CJK JP' , weight = 'Medium'},
---     { family = 'Noto Sans CJK SC' , weight = 'Medium'},
---     { family = 'Noto Sans CJK TC' , weight = 'Medium'},
---     { family = 'Noto Sans CJK KR' , weight = 'Medium'},
---     'Noto Color Emoji'
--- }
 config.font = wezterm.font_with_fallback {
-    { family = 'Maple Mono NL NF CN', weight = "Medium" },
+    { family = 'FantasqueSansM Nerd Font Mono' , weight = 'Regular' },
+    { family = 'Noto Sans CJK JP' , weight = 'Medium'},
+    { family = 'Noto Sans CJK SC' , weight = 'Medium'},
+    { family = 'Noto Sans CJK TC' , weight = 'Medium'},
+    { family = 'Noto Sans CJK KR' , weight = 'Medium'},
     'Noto Color Emoji'
 }
-config.font_size = 13
+config.font_size = 14
 config.hide_tab_bar_if_only_one_tab = true
 config.initial_cols = 120
 config.initial_rows = 36
