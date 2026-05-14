30d7829a808b31639eb77ed978ee8294f5dafd17
fantaro <fantaro@gmail.com>
2026-05-14 21:09:49 +0900

    update

---
+++ .config/nvim/lua/plugins/noice.lua
@@ -0,0 +1,18 @@
+-- folke/noice.nvim
+return {
+  "folke/noice.nvim",
+  opts = {
+    views = {
+      cmdline_popup = {
+        position = {
+          row = "70%",
+          col = "50%",
+        },
+        size = {
+          width = "60%",
+          height = "auto",
+        },
+      },
+    },
+  },
+}
